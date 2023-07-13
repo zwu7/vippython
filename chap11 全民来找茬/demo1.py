ef084b5cef2096cd06bd134c6d41b410121328f4
zwu <zwu7@nd.edu>
2023-07-13 18:17:18 -0400

    chap11

---
+++ chap11 全民来找茬/demo1.py
@@ -0,0 +1,11 @@
+age = input('请输入你的年龄：')
+print(type(age))
+
+if int(age) >= 18:
+    print('成年人...')
+
+
+i = 0
+while i < 10:
+    print(i)
+    i += 1
---
+++ chap11 全民来找茬/demo2.py
@@ -0,0 +1,10 @@
+lst = [11, 22, 33, 44]
+# print(lst[4])
+print(lst[3])
+
+lst = []
+# lst = append(['A', 'B', 'C'])
+lst.append('A')
+lst.append('B')
+lst.append('C')
+print(lst)
