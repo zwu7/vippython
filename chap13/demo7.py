f09829b1df6e97452a3ea2e7336945af0eb93085
zhenyuwu <zwu4danniel@gmail.com>
2023-07-27 16:17:01 -0400

    chap13

---
+++ chap13/demo7.py
@@ -0,0 +1,32 @@
+class Animal(object):
+    def eat(self):
+        print('动物要吃东西')
+
+
+class Dog(Animal):
+    def eat(self):
+        print('狗吃骨头...')
+
+
+class Cat(Animal):
+    def eat(self):
+        print('猫吃鱼...')
+
+
+class Person:
+    def eat(self):
+        print('人吃五谷杂粮...')
+
+
+# 定义一个函数
+def fun(obj):
+    obj.eat()
+
+
+# 开始调用函数
+fun(Cat())
+fun(Dog())
+fun(Animal())
+
+print('-' * 30)
+fun(Person())
