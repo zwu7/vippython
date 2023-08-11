266291e82b3523fc6aa16d7204d42968f453d62b
zwu <zwu7@nd.edu>
2023-08-11 07:34:43 -0400

    studentsys project

--- studentsys/stusystem.py
+++ studentsys/stusystem.py
@@ -1,7 +1,12 @@
+import os
+
+filename = 'student.txt'
+
+
 def main():
     while True:
         menu()
-        choice = int(input('请选择'))
+        choice = int(input('请选择\n'))
         if choice in range(8):
             if choice == 0:
                 answer = input('您确定要退出系统吗？y/n')
@@ -41,7 +46,45 @@ def menu():
 
 
 def insert():
-    pass
+    student_list = []
+    while True:
+        id = input('请输入ID（如1001）：')
+        if not id:
+            break
+        name = input('请输入姓名：')
+        if not name:
+            break
+
+        try:
+            english = int(input('请输入英语成绩：'))
+            python = int(input('请输入Python成绩：'))
+            java = int(input('请输入Java成绩：'))
+        except:
+            print('输入无效，不是整数类型，请重新输入：')
+            continue
+        # 将录入的学生信息保存到字典中
+        student = {'id': id, 'name': name, 'english': english, 'python': python, 'java': java}
+        # 将学生信息添加到列表中
+        student_list.append(student)
+        answer = input('是否继续添加？y/n\n')
+        if answer == 'y':
+            continue
+        else:
+            break
+
+    # 调用save()函数
+    save(student_list)
+    print('学生信息录入完毕！！！')
+
+
+def save(lst):
+    try:
+        stu_txt = open(filename, 'a', encoding='utf-8')
+    except:
+        stu_txt = open(filename, 'w', encoding='utf-8')
+    for item in lst:
+        stu_txt.write(str(item) + '\n')
+    stu_txt.close()
 
 
 def search():
@@ -49,11 +92,43 @@ def search():
 
 
 def delete():
-    pass
+    while True:
+        student_id = input('请输入要删除的学生的id：')
+        if student_id != '':
+            if os.path.exists(filename):
+                with open(filename, 'r', encoding='utf-8') as file:
+                    student_old = file.readlines()
+            else:
+                student_old = []
+            flag = False  # 标记是否删除
+            if student_old:
+                with open(filename, 'w', encoding='utf-8') as wfile:
+                    d = {}
+                    for item in student_old:
+                        dict(eval(item))  # 将字符串转成字典
+                        if d['id'] != student_id:
+                            wfile.write(str(d) + '\n')
+                        else:
+                            flag = True
+                    if flag:
+                        print(f'ID为{student_id}的学生信息已被删除')
+                    else:
+                        print(f'没有找到ID为{student_id}的学生信息')
+            else:
+                print('无学生信息')
+                break
+            show()  # 删除之后要重新显示所有学生信息
+            answer = input('是否继续删除？y/n\n')
+            if answer == 'y':
+                continue
+            else:
+                break
 
 
 def modify():
-    pass
+    show()
+    if os.path.exists(filename):
+        with open(file, 'r', encoding='utf-8') as rfile:
 
 
 def sort():
